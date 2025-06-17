4b8f5dfa63d323e4b50df9e6aba41d461e737e52
Rodrigo Lin <alexisrodrigolin@gmail.com>
2025-06-17 10:55:13 -0300

    feat:freeRtos Class

---
+++ workspace_lpc845/clase_03/armgcc/debug/CMakeFiles/4.0.1/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Darwin-24.5.0")
+set(CMAKE_HOST_SYSTEM_NAME "Darwin")
+set(CMAKE_HOST_SYSTEM_VERSION "24.5.0")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "arm64")
+
+include("/Users/rok/Documents/curso-lse/workspace_lpc845/sdks/clase_03_sdk/tools/cmake_toolchain_files/armgcc.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
