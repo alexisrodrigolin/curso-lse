83ab23e6b65a4a2ca7059c8d37617bdb1a1e1d72
Rodrigo Lin <alexisrodrigolin@gmail.com>
2025-07-08 14:02:53 -0300

    feat: make trabajo_integrador

---
+++ trabajo_integrador/integrador/isr.h
@@ -0,0 +1,15 @@
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "semphr.h"
+
+#include "wrappers.h"
+
+// Semaforos y colas externas
+
+extern xQueueHandle queue_adc;
+extern xSemaphoreHandle semphr_buzz;
+
+
+// Prototipos de callbacks
+
+void cny70_callback(pint_pin_int_t pintr, uint32_t pmatch_status);
